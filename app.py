43b8c765a9e6c194464c556ae65b901dd573d986
sayendranadh <118617049+sayendranadh@users.noreply.github.com>
2025-06-14 20:50:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import gradio as gr
+from utils.inference import predict_condition
+
+with gr.Blocks() as demo:
+    gr.Markdown("# 💛 Skin Cancer Detection Chatbot")
+
+    with gr.Row():
+        image_input = gr.Image(type="numpy", label="Upload Skin Lesion Image")
+        output_text = gr.Textbox(label="Prediction & Info")
+
+    submit_btn = gr.Button("Diagnose")
+    submit_btn.click(fn=predict_condition, inputs=image_input, outputs=output_text)
+
+demo.launch()
 
